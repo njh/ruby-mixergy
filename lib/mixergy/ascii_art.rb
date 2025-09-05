ad813735b93f509cb90a78b212924232fa8ca623
Nicholas Humfrey <njh@aelius.com>
2025-09-05 21:42:29 +0100

    Added black and white ASCII Art of the cylinder charge

---
+++ lib/mixergy/ascii_art.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Mixergy
+  module AsciiArt
+    # Draws an ASCII art hot water cylinder representing a charge level.
+    # @param charge [Numeric] The charge percentage (0-100).
+    # @param width [Integer] The width of the tank (default: 7).
+    # @param height [Integer] The height of the tank (default: 10).
+    # @return [String] The ASCII art representation of the tank.
+   def self.draw_tank(charge, width: 7, height: 10)
+      fill_height = (height * charge / 100.0).round
+
+      tank = []
+      tank << " ╭#{'─' * width}╮"
+      height.times do |i|
+        if i >= height - fill_height
+          tank << " │#{'█' * width}│"
+        else
+          tank << " │#{' ' * width}│"
+        end
+      end
+      tank << " ╰#{'─' * width}╯"
+      tank.join("\n")
+    end
+  end
+end
